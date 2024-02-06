edc9e5d2fca354147f129e7a45481460e477d1ed
Aleksandr Maleta <a.maleta@dats.team>
2024-02-06 23:43:47 +0100

    fix db 3

--- src/main/java/com/example/DeliveryApp/entity/Product.java
+++ src/main/java/com/example/DeliveryApp/entity/Product.java
@@ -3,9 +3,12 @@ package com.example.DeliveryApp.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import io.hypersistence.utils.hibernate.type.json.JsonType;
 
 import java.util.List;
+import org.hibernate.annotations.Type;
 
 @Data
 @Entity
@@ -20,9 +23,17 @@ public class Product {
     @Column(name = "id")
     private Long id;
     @Column(name = "parametrs")
-    private String parameter;
+    @Type(JsonType.class)
+    private Parameter parameter;
 
     @ManyToMany
     @JoinTable(name = "order_product", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "order_id"))
     private List<Sale> sales;
+
+
+    @Getter
+    @AllArgsConstructor
+    public static class Parameter {
+
+    }
 }
