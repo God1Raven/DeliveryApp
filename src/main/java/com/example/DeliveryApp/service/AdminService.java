dbda362ccc5d39ad7118ebae044c8863483353b5
Aleksandr Maleta <a.maleta@dats.team>
2024-02-06 23:36:28 +0100

    fix db

--- src/main/java/com/example/DeliveryApp/service/AdminService.java
+++ src/main/java/com/example/DeliveryApp/service/AdminService.java
@@ -3,20 +3,16 @@ package com.example.DeliveryApp.service;
 import com.example.DeliveryApp.dto.AdminDto;
 import com.example.DeliveryApp.mapper.AdminMapper;
 import com.example.DeliveryApp.repository.AdminRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-
+@RequiredArgsConstructor
 public class AdminService implements BaseService<AdminDto>{
-    private AdminRepository adminRepository;
-    private AdminMapper adminMapper;
 
-    @Autowired
-    public AdminService(AdminRepository adminRepository, AdminMapper adminMapper) {
-        this.adminRepository = adminRepository;
-        this.adminMapper = adminMapper;
-    }
+    private final AdminRepository adminRepository;
+    private final AdminMapper adminMapper;
 
     public AdminDto create(AdminDto adminDto) {
         return adminMapper.mapEntityToDto(adminRepository.save(adminMapper.mapDtoToEntity(adminDto)));
