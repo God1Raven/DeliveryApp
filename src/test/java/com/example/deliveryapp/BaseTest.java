a4727a8d10c81e989daa70676bcbcc8d6bd6e36c
Raven <Noymov@mail.ru>
2024-04-26 15:09:58 +0200

    Fix tests

--- src/test/java/com/example/deliveryapp/BaseTest.java
+++ src/test/java/com/example/deliveryapp/BaseTest.java
@@ -7,12 +7,34 @@ import com.example.deliveryapp.security.User;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest(classes = {IntegractionTestConfiguration.class, JunitTestConfiguration.class})
 @ActiveProfiles("test")
 public class BaseTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16.2")
+            .withNetworkAliases("delivery-app-network").withUsername("postgres");
+
+    @DynamicPropertySource
+    public static void changeConfiguration(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> postgreSQLContainer.getJdbcUrl() + "&stringtype=unspecified");
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        postgreSQLContainer.start();
+    }
+
     User user = new User(1L, "test", "test", "test@gmail.com", Role.CLIENT);
 
     public static String getContentFromFile(final String fileName) {
--- src/test/java/com/example/deliveryapp/DatabaseTest.java
+++ src/test/java/com/example/deliveryapp/DatabaseTest.java
@@ -1,25 +1,8 @@
 package com.example.deliveryapp;
 
 import com.github.database.rider.spring.api.DBRider;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @DBRider
-@Testcontainers
 public class DatabaseTest extends BaseTest {
-
-    @Container
-    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16.2")
-            .withUsername("postgres");
-
-    @DynamicPropertySource
-    public static void changeConfiguration(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasourceutils.url", () -> postgreSQLContainer.getJdbcUrl() + "&stringtype=unspecified");
-        registry.add("spring.datasourceutils.username", postgreSQLContainer::getUsername);
-        registry.add("spring.datasourceutils.password", postgreSQLContainer::getPassword);
-    }
 
 }
